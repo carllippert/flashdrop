541a0919aa0d7583c483c9eb8489bf5ceb99770c
lesleyhiggins <53590776+lesleyhiggins@users.noreply.github.com>
2022-01-15 23:05:43 -0700

    UI button updates

--- components/Minter.js
+++ components/Minter.js
@@ -1,19 +1,51 @@
 import { useEffect, useState } from "react";
-import { connectWallet } from "./utils/interact.js";
- 
+import {
+  connectWallet,
+  getCurrentWalletConnected,
+  mintNFT,
+} from "./util/interact.js";
 
 const Minter = (props) => {
-
-  //State variables
   const [walletAddress, setWallet] = useState("");
   const [status, setStatus] = useState("");
+
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [url, setURL] = useState("");
- 
-  useEffect(async () => { //TODO: implement
-    
+
+  useEffect(async () => {
+    const { address, status } = await getCurrentWalletConnected();
+
+    setWallet(address);
+    setStatus(status);
+
+    addWalletListener();
   }, []);
+
+  function addWalletListener() {
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", (accounts) => {
+        if (accounts.length > 0) {
+          setWallet(accounts[0]);
+          setStatus("👆🏽 Write a message in the text-field above.");
+        } else {
+          setWallet("");
+          setStatus("🦊 Connect to Metamask using the top right button.");
+        }
+      });
+    } else {
+      setStatus(
+        <p>
+          {" "}
+          🦊{" "}
+          <a target="_blank" href={`https://metamask.io/download.html`}>
+            You must install Metamask, a virtual Ethereum wallet, in your
+            browser.
+          </a>
+        </p>
+      );
+    }
+  }
 
   const connectWalletPressed = async () => {
     const walletResponse = await connectWallet();
@@ -21,11 +53,17 @@ const Minter = (props) => {
     setWallet(walletResponse.address);
   };
 
-
-  const onMintPressed = async () => { //TODO: implement
-    
+  const onMintPressed = async () => {
+    const { success, status } = await mintNFT(url, name, description);
+    setStatus(status);
+    if (success) {
+      setName("");
+      setDescription("");
+      setURL("");
+    }
   };
 
+  
   return (
     <div className="Minter p-10">
       <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-4 px-8 rounded space-y-3.5" id="walletButton" onClick={connectWalletPressed}>
